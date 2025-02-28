3748bd61eb4e74e2512e7256c2484a21fa5401ed
DyMColaborateGit <git@dymsites.co>
2025-02-28 17:02:20 -0500

    28-02-2025

--- App.Logic/obj/Release/net7.0/App.logic.AssemblyInfo.cs
+++ App.Logic/obj/Release/net7.0/App.logic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486fde83408c67693673dd7afcbb5265e9e559d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6af2b97c0b8ed5bb3661ab56b8dec9a1a9ffd46")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- App.Models/obj/Release/net6.0/App.Models.AssemblyInfo.cs
+++ App.Models/obj/Release/net6.0/App.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486fde83408c67693673dd7afcbb5265e9e559d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6af2b97c0b8ed5bb3661ab56b8dec9a1a9ffd46")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
