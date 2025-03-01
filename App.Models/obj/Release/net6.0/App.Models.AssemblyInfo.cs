e0bcf7e8eaa700c1e26ed571d247ceadcc1e49cb
John M. Castañeda <jhoncastaneda128@gmail.com>
2025-03-01 12:22:11 -0500

    01-03-2025

--- App.Models/obj/Release/net6.0/App.Models.AssemblyInfo.cs
+++ App.Models/obj/Release/net6.0/App.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6af2b97c0b8ed5bb3661ab56b8dec9a1a9ffd46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3748bd61eb4e74e2512e7256c2484a21fa5401ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
