7d129bd8613924e9be1b41adc4528cab723c0042
John M. Castañeda <jhoncastaneda128@gmail.com>
2025-04-20 22:11:29 -0500

    20-04-2025

--- App.Models/obj/Release/net6.0/App.Models.AssemblyInfo.cs
+++ App.Models/obj/Release/net6.0/App.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94178803d629553a3432b26237763c722ec6c886")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba241ec7030beb05da848d7814882083618edf26")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Qplus/obj/Release/net7.0/Qplus.AssemblyInfo.cs
+++ Qplus/obj/Release/net7.0/Qplus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94178803d629553a3432b26237763c722ec6c886")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba241ec7030beb05da848d7814882083618edf26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
