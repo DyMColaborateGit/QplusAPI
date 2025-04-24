c7cedb5801022093c33f83667b9e089e1f5456c5
John M. Castañeda <jhoncastaneda128@gmail.com>
2025-04-24 16:43:04 -0500

    24-04-2025

--- App.Logic/obj/Release/net7.0/App.logic.AssemblyInfo.cs
+++ App.Logic/obj/Release/net7.0/App.logic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba241ec7030beb05da848d7814882083618edf26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d129bd8613924e9be1b41adc4528cab723c0042")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
