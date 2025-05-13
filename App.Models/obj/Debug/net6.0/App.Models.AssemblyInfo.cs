897e8811ba17407d1a138ab7a0af1bbb8849a640
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-05-13 10:27:38 -0500

    13-05-2025

--- App.Models/obj/Debug/net6.0/App.Models.AssemblyInfo.cs
+++ App.Models/obj/Debug/net6.0/App.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880f79935b2008ee4bfec31c19428e744847d0eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adbb6de17ed4574ed9c78c4e1e86a213afbc24a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
