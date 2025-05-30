6d53b4c99df4797340286265a162c76ce5036176
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-05-30 16:56:27 -0500

    30-05-2025

--- App.Logic/obj/Debug/net7.0/App.logic.AssemblyInfo.cs
+++ App.Logic/obj/Debug/net7.0/App.logic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1226efad309119f73677ec43462a60a426b0b045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35265b5f356263dfd9e87af7118900dd7f869b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
