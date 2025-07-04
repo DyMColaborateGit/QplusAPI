79d04cc7f39938654d629340ade5d4a1de9fda52
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-07-04 10:41:51 -0500

    04-07-2025
    Ajustes Mover Archivos

--- App.Models/obj/Debug/net6.0/App.Models.AssemblyInfo.cs
+++ App.Models/obj/Debug/net6.0/App.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce1295fe76ab343fa21bdaeb05eb40458b8155b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82054c006da3c16286af1148677245d45f9f6d75")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
