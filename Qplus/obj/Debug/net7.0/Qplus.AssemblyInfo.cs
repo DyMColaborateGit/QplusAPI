73abd6ebf76d2ce0f7f78a48191b06a1fb8b0a86
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-07-31 08:42:55 -0500

    31-07-2025

--- Qplus/obj/Debug/net7.0/Qplus.AssemblyInfo.cs
+++ Qplus/obj/Debug/net7.0/Qplus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e95cdc524cd451db48d82015e79ccccae33d7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e9464a934d2dbfae2209bcdc37dc33d1d0ad7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
