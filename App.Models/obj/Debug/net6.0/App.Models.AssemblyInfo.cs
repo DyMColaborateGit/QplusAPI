1120ad18df602f326e18e5e294d45fe61ca968fc
GermanDev320 <german.valencia@outlook.com>
2025-08-22 15:43:59 -0500

    ajustes

--- App.Models/obj/Debug/net6.0/App.Models.AssemblyInfo.cs
+++ App.Models/obj/Debug/net6.0/App.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b23cb8a82341d2bb623f3acf29558510c76650")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590d5178a72ede145590b95fd1a16a9a6b1825a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
