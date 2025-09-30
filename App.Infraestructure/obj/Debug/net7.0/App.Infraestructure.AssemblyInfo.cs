043679bc77af534849a52e72d040ba712dfbb1e8
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-09-30 16:59:49 -0500

    Ajustes JM - 30-09-2025

--- App.Infraestructure/obj/Debug/net7.0/App.Infraestructure.AssemblyInfo.cs
+++ App.Infraestructure/obj/Debug/net7.0/App.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f54bfac985f5f4e7a00d6c88bee06f93f79779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ab634db2d837b7fddb9c616a0219cbeae624d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
