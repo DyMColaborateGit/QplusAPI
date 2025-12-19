61ca5c52e11d0ee2d5b23306a81189e6b4bc634d
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-12-19 16:32:21 -0500

    19-12-2025

--- Qplus/obj/Release/net7.0/Qplus.AssemblyInfo.cs
+++ Qplus/obj/Release/net7.0/Qplus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e24fad9db18cf6efa181c765ec8783f995caeac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d521da538dbbeb44e22fd15c271c6479b129ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
