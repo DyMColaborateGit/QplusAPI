355fa291a43ab831bef9878912bdaff05b4801df
JohnCastañeda <jhoncastaneda128@gmail.com>
2026-01-22 15:00:10 -0500

    JM-Ajuste-22-01-2026

--- App.Logic/IServices/IActividadesPIDService.cs
+++ App.Logic/IServices/IActividadesPIDService.cs
@@ -12,6 +12,7 @@ namespace App.logic.IServices
     {
         Task<TBL_com_ActividadesPIDModels> GetObjActividadesPDI(int EmpresaId);
         Task<List<TBL_com_ActividadesPIDModels>> GetListaActividadesPDI(int EmpresaId);
+        Task<List<TBL_com_ActividadesPIDModels>> GetListaActividadesPDIByEvaluadoIdByAnio(int EmpresaId, long EvaluadoId, int InAnio);
         Task<TBL_com_ActividadesPIDModels> PostCreatenewActividadPID(TBL_com_ActividadesPIDModels ObjRequest);
 
     }
